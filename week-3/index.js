9a1c4868bb6b2740a41b637e5f40ec696cac0df7
finnzzzz <o8786791@gmail.com>
2023-04-13 16:23:26 +0800

    week-3

---
+++ week-3/index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("<h2 style='color: #ff7621;'>Hello AppWorksSchool</h2>");
+});
+
+app.use(express.static("public"));
+
+app.post("/getData", (req, res) => {
+  const number = req.body.number;
+  if (number) {
+    if (isNaN(number)) {
+      res.send({ result: "Wrong Parameter" });
+    } else {
+      // -----------v1
+      let result = [];
+      for (let i = 1; i <= number; i++) {
+        result.push(i);
+      }
+      res.send({ result: result.join("+") });
+
+      //-----------v2
+      // let result = "";
+      // for (let i = 1; i <= number; i++) {
+      //   result += i;
+      //   if (i !== parseInt(number)) {
+      //     result += "+";
+      //   }
+      // }
+      // res.send({ result: result });
+    }
+  } else {
+    res.send({ result: "Lack of Parameter" });
+  }
+});
+
+app.get("/getData", (req, res) => {
+  const number = req.query.number;
+  if (number) {
+    if (isNaN(number)) {
+      res.send("Wrong Parameter");
+    } else {
+      //-----------v1
+      // let result = [];
+      // for (let i = 1; i <= number; i++) {
+      //   result.push(i);
+      // }
+      // res.send(result.join('+'))
+
+      //-----------v2
+      let result = "";
+      for (let i = 1; i <= number; i++) {
+        result += i;
+        if (i !== parseInt(number)) {
+          result += "+";
+        }
+      }
+      res.send(result);
+    }
+  } else {
+    res.send("Lack of Parameter");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`The app is running on localhost: ${port}`);
+});
